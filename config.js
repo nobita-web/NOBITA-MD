c34f1acaf8750c2d65af38f1d451fac77605afa1
nobita-web <rohitroy742213@gmail.com>
2026-01-01 23:02:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -38,6 +38,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 NOBITA MD  』",
+BOT_NAME: process.env.BOT_NAME || "『NOBITA MD』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
